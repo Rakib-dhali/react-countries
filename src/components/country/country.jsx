0ea562ab1dc3812c0e4fd81ce4f1e2fc6cf55c40
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-03-14 23:20:02 +0600

    5

--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
 import './Country.css'
 
 const Country = ({ country }) => {
-    console.log(country.population.population, country.area.area);
+    const [visited, setVisited] = useState(false)
+    const handleClick  = () => {
+        setVisited(!visited)
+    }
   return (
     <div className="country">
       <img
@@ -11,9 +15,10 @@ const Country = ({ country }) => {
       />
     <h3>{country.name.common}</h3>
     <p>population: {country.population.population}</p>
-    <p>are: {country.area.area}</p>
+    <p>area: {country.area.area} {country.area.area > 147570? "big country": "small country" }</p>
+    <button className={`${visited===true? "green": "ash"}`} onClick={handleClick}>{visited===true? "visited": "not applied"}</button>
     </div>
   )
 }
 
-export default Country
+export default Country 
