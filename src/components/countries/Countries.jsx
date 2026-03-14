1fdcce0cb86c99ea1b3ce01396cb4495d6c2f9df
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-03-14 22:55:25 +0600

    4

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,18 +1,12 @@
 import { use } from "react";
 import Country from "../country/country";
+import './Countries.css'
 
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 400px)",
-        gap: "10px",
-        marginTop: "20px",
-      }}
-    >
+    <div className="countries">
       {countries.map((country) => (
         <Country country={country} key={country.ccn3.ccn3}/>
       ))}
--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,12 +1,17 @@
+import './Country.css'
+
 const Country = ({ country }) => {
+    console.log(country.population.population, country.area.area);
   return (
-    <div style={{ display: "flex", flexDirection: "row", gap: "10px", alignItems: "center" }}>
+    <div className="country">
       <img
         
         src={country.flags.flags.png}
         alt={country.flags.flags.alt}
       />
     <h3>{country.name.common}</h3>
+    <p>population: {country.population.population}</p>
+    <p>are: {country.area.area}</p>
     </div>
   )
 }
