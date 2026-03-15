b26396735b11aea0877d34d519ab1f0a363d8ef1
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-03-15 11:58:39 +0600

    7

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -3,20 +3,21 @@ import Country from "../country/country";
 import './Countries.css'
 
 const Countries = ({ countriesPromise }) => {
-  // const [visitedCountries, setVisitedCountries] = useState([])
+  const [visitedCountries, setVisitedCountries] = useState([])
 
   const handleVisitedCountries = (country)=>{
-    console.log(country);
-    
+    const newVisitedCountry = [...visitedCountries, country]    
+    setVisitedCountries(newVisitedCountry)
   }
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
-  return (
+  return (<>
+  <h2>visited country {visitedCountries.length}</h2>
     <div className="countries">
       {countries.map((country) => (
         <Country country={country} key={country.ccn3.ccn3} handleVisitedCountries={handleVisitedCountries}/>
       ))}
-    </div>
+    </div></>
   );
 };
 
